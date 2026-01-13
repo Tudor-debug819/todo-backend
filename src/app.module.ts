ad2852775c0602c4accef4fca54c636eed1ba7c0
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2026-01-13 13:46:41 +0200

    asfsfasf

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TasksModule } from './tasks/tasks.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // <- citește .env
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',       
-      password: 'Integrat2025',       
-      database: 'todoapp',        
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true,
-      synchronize: true,          // DOAR LOCAL!
+      synchronize: true,
+      ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
     }),
     TasksModule,
   ],
